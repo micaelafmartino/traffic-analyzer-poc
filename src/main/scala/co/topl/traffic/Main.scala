92e80cae818a31189f99d3aa1e10c2f0429c7ab8
Micaela Martino <8515210+micaelafmartino@users.noreply.github.com>
2022-07-14 15:58:29 -0300

    Graphviz DOT file generator for graph plotting and SVGs

--- src/main/scala/co/topl/traffic/Main.scala
+++ src/main/scala/co/topl/traffic/Main.scala
@@ -20,11 +20,12 @@ import co.topl.traffic.utils.extensions.JsValueExtensions
 object Main extends App {
   val url = """file:src/dist/sample-data.json"""
   val measurements: List[TrafficMeasurement] = TrafficMeasurements.readMeasurements(url).get.trafficMeasurements.head.measurements
+  val segments = measurements.map(_.segment)
   val json = TrafficMeasurements.readMeasurementsJson(url).get.prettyString()
-  val adjacencies: Map[Intersection, List[RoadSegment]] = measurements.map(_.segment).groupBy(_.start)
+  val adjacencies: Map[Intersection, List[RoadSegment]] = segments.groupBy(_.start)
 
-  val intersections: Set[Intersection] = measurements.map(_.segment).flatMap(m => List(m.start, m.end)).toSet
-  val leadingSegments: LeadingSegments = measurements.map(_.segment).groupBy(_.end)
+  val intersections: Set[Intersection] = segments.flatMap(m => List(m.start, m.end)).toSet
+  val leadingSegments: LeadingSegments = segments.groupBy(_.end)
 
   println(TrafficAnalyzer(leadingSegments, intersections.last).shortestPathFrom(intersections.head))
   println()
@@ -36,4 +37,53 @@ object Main extends App {
   println("with only 1 possible path: " + adjacencies.count(_._2.size == 1))
   println("with 2 possible paths: " + adjacencies.count(_._2.size == 2))
   //  adjacencies.mapValues(_.size).filter(_._2 < -1).foreach(println)
+}
+
+/** DOT file generator to plot the graph using Graphviz */
+object GraphvizGenerator extends App {
+  val url = """file:src/dist/sample-data.json"""
+  val segments: List[RoadSegment] = TrafficMeasurements
+    .readMeasurements(url).get
+    .trafficMeasurements.head
+    .measurements
+    .map(_.segment)
+
+  val intersections = segments.flatMap(m => List(m.start, m.end)).toSet
+
+  val intersectionsMatrix: List[List[Intersection]] = intersections
+    .groupBy(_.street.toInt)
+    .view.mapValues(_.toList.sortBy(_.avenue))
+    .toList.sortBy(_._1)
+    .map(_._2)
+
+  val horizontalGrid = intersectionsMatrix
+    .map(_.mkString("rank=same {", " -> ", "}"))
+    .mkString("\n")
+
+  val verticalGrid = intersectionsMatrix
+    .transpose
+    .map(_.mkString(" -> "))
+    .mkString("\n")
+
+  val edges: String = segments
+    .map(s => s""""${s.start}" -> "${s.end}" [label = "${s.transitTime.round}"]""")
+    .mkString("\n")
+
+  val graphvizCode =
+    s"""
+       |digraph {
+       |node [shape = plaintext]
+       |splines = false
+       |
+       |$edges
+       |
+       |edge [style=invis]
+       |
+       |$verticalGrid
+       |
+       |$horizontalGrid
+       |}
+       |""".stripMargin
+
+  println(graphvizCode)
 }
--- src/main/scala/co/topl/traffic/model/cityNetwork/package.scala
+++ src/main/scala/co/topl/traffic/model/cityNetwork/package.scala
@@ -2,8 +2,12 @@ package co.topl.traffic.model
 
 package object cityNetwork {
   /** Intersection between and Avenue and a Street (in a grid pattern) */
-  case class Intersection(street: String, avenue: String)
+  case class Intersection(street: String, avenue: String) {
+    override def toString: String = s"$avenue$street"
+  }
 
   /** One-way segment of the road bewteen two intersections with the time it takes to traverse it */
-  case class RoadSegment(transitTime: Double, start: Intersection, end: Intersection)
+  case class RoadSegment(transitTime: Double, start: Intersection, end: Intersection) {
+    override def toString: String = s"$start -> $end [$transitTime]"
+  }
 }
